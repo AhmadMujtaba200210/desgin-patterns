79d7dec589c6079e66e34752ec61e107809f622a
AhmadMujtaba200210 <cadeta6@gmail.com>
2023-04-05 19:24:50 +0500

    fix: WindowsCheckBox sout fixed

--- patterns/src/creational/abstractFactory/checkbox/WindowsCheckBox.java
+++ patterns/src/creational/abstractFactory/checkbox/WindowsCheckBox.java
@@ -3,6 +3,6 @@ package creational.abstractFactory.checkbox;
 public class WindowsCheckBox implements CheckBox{
     @Override
     public void paint() {
-            System.out.println("You're printing MacOS CheckBox");
+            System.out.println("You're printing Windows CheckBox");
     }
 }
